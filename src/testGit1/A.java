edf6e4e076561a07969f456cbee55d561f2f230b
wanglt2014 <wang_lt@SDC-C-wanglitao5645>
2015-12-14 13:35:28 +0800

    2C

--- src/testGit1/A.java
+++ src/testGit1/A.java
@@ -9,7 +9,7 @@ public class A {
 
 		System.out.println("this is testGit2 commit");
 
-		System.out.println("this is testGit1 commit2");
+		System.out.println("this is testGit1 commit222222");
 
 	}
 
