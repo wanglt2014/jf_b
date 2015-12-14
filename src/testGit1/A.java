b71a8ebdc0a566ab9ffaaa2e88a9534da9aa8ee5
wanglt2014 <wang_lt@SDC-C-wanglitao5645>
2015-12-14 14:23:31 +0800

    2F

--- src/testGit1/A.java
+++ src/testGit1/A.java
@@ -6,7 +6,7 @@ public class A {
 		// TODO Auto-generated method stub
 		System.out.println("this is testGit1 commit");
 		// 2
-
+		System.out.println("this is testGit2 commit222222222222222");
 		System.out.println("this is testGit2 commit21");
 
 		System.out.println("this is testGit1 commit222222222221");
