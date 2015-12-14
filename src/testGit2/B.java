932fc3f9d4d1c936046a67c01742a8bcb3ef3359
wanglt2014 <wang_lt@SDC-C-wanglitao5645>
2015-12-14 14:33:54 +0800

    1J

--- src/testGit2/B.java
+++ src/testGit2/B.java
@@ -4,7 +4,8 @@ public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2");
+		System.out.println("1");
+		System.out.println("1");
 	}
 
 }
